cd7db74782067c125cb9cc34004203ce46a499e3
aldeir <aldeir@r2soft.com.br>
2022-11-22 17:06:45 -0300

    first commit

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization
+{
+
+    public $id = 1;
+
+    public $name = 'Deir';
+
+    public $site = 'youtube.com.br';
+
+    public $description = 'LoremLorem ipsum dolor sit amet consectetur adipisicing
+     elit. Nam dolor deleniti deserunt sit similique voluptatum ut quis natus dolore 
+     placeat, maxime aperiam, repellat ex voluptatem tempora, ipsa consectetur dolorem quia?';
+}
